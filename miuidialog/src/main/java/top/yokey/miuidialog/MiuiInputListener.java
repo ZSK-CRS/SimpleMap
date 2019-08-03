10d1fd4d12853f01ecbd8e12fc4e407e53dc7f25
ZSK-CRS <1625127317@qq.com>
2019-08-03 14:00:10 +0800

    修改bug

---
+++ miuidialog/src/main/java/top/yokey/miuidialog/MiuiInputListener.java
@@ -0,0 +1,10 @@
+package top.yokey.miuidialog;
+
+import android.app.Dialog;
+
+@SuppressWarnings("ALL")
+public interface MiuiInputListener {
+
+    void onClick(String content, Dialog dialog);
+
+}
